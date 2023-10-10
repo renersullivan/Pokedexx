f8edc6cb05b239fe5f9d898a0499560e91973b18
renersullivan1 <renersullivan1@email.com>
2023-10-10 19:38:17 -0300

    adicionando url API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
@@ -14,7 +14,9 @@ import { HomeComponent } from './home/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+export type listResults = Pokemon[]
+
+export interface Pokemon {
+  name: string
+  url: string
+}
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public results!:listResults
+
+  constructor(private http:HttpClient){
+
+  }
+  
+ public ngOnInit():void{
+  this.http.get<any>('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0').subscribe(data=>{
+    this.results = data.results
+  })
+  console.log('nasceu !')
+    
+  }
+  
 
 }
+
