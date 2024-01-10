8130c9d4c5690211fda0d91dff252bde004e3739
renersullivan1 <renersullivan1@email.com>
2024-01-10 16:31:39 -0300

    atualizando as url das api

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
 export type listResults = Pokemon[]
 
 export interface Pokemon {
+imageURL: any |undefined;
   name: string
   url: string
 }
@@ -15,6 +16,7 @@ export interface Pokemon {
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  pokemon:[] | undefined
   public results!:listResults
 
   constructor(private http:HttpClient){
@@ -26,14 +28,22 @@ export class HomeComponent implements OnInit {
 
   }
 
-  public buscaPokemons():void{
-    this.http.get<any>('https://pokeapi.co/api/v2/pokemon?limit=151').subscribe(data=>{
-      this.results = data.results
-      console.log(data.results)
-    })
-
+  public buscaPokemons(): void {
+    this.http.get<any>('https://pokeapi.co/api/v2/pokemon?limit=151').subscribe(data => {
+      this.results = data.results.map((pokemon: any) => {
+        const pokemonId = this.extractIdFromUrl(pokemon.url); // Extrai o ID do Pokémon da URL
+        pokemon.imageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`;
+        return pokemon;
+      });
+      console.log(this.results);
+    });
   }
-
+  
+  private extractIdFromUrl(url: string): number {
+    const parts = url.split('/').filter(part => part.trim() !== '');
+    return parseInt(parts[parts.length - 1], 10);
+  }
+  
   public clickBuscaPokemonDetalhe(poke:Pokemon):void{
    
     }
